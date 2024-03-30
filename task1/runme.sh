796dea318b1615a385d83da5d2984173bdfe5746
STPDRichie <nikolaj.karpov.2002@mail.ru>
2024-03-30 19:29:46 +0500

    в runme.sh перенаправил вывод stat в result.txt и вызвал вверху make clean вместо rm A и B. в Makefile clean добавил rm result.txt

--- task1/runme.sh
+++ task1/runme.sh
@@ -1,10 +1,9 @@
-rm A 2>/dev/null
+make clean
+
 fallocate -l 4194305 A
 
 gcc makeA.c -o makeA
 ./makeA
-
-rm B 2>/dev/null
 
 gcc sparse.c -o sparse
 
@@ -17,9 +16,9 @@ gzip -cd B.gz | ./sparse C
 
 ./sparse A D 100
 
-echo "Размер A: $(stat -c %s A) байт"
-echo "Размер B: $(stat -c %s B) байт"
-echo "Размер A.gz: $(stat -c %s A.gz) байт"
-echo "Размер B.gz: $(stat -c %s B.gz) байт"
-echo "Размер C: $(stat -c %s C) байт"
-echo "Размер D: $(stat -c %s D) байт"
+echo "Размер A: $(stat -c %s A) байт" >> ./result.txt
+echo "Размер B: $(stat -c %s B) байт" >> ./result.txt
+echo "Размер A.gz: $(stat -c %s A.gz) байт" >> ./result.txt
+echo "Размер B.gz: $(stat -c %s B.gz) байт" >> ./result.txt
+echo "Размер C: $(stat -c %s C) байт" >> ./result.txt
+echo "Размер D: $(stat -c %s D) байт" >> ./result.txt
