7e464372af75a574c4fbe31a235ba307b0b9fe17
STPDRichie <nikolaj.karpov.2002@mail.ru>
2024-04-18 02:48:06 +0500

    получение pid из lck-файла вынес в функцию get_lck_file_str_pid. добавил sigint_handler, в котором записывается количество блокировок в result.txt и удаляется lck-файл (если он от текущего процесса), привязал его через signal к SIGINT

--- task2/lock_program.c
+++ task2/lock_program.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <signal.h>
 
 const int LOCK_TIME = 1;
 
@@ -10,11 +11,50 @@ const int PID_SIZE = sizeof(pid_t);
 
 const int MAX_FILENAME_LENGTH = 255*8;
 
+int locks_count = 0;
+
+char* get_lck_file_str_pid(char* lck_file_name) {
+    int lck_file = open(lck_file_name, O_RDONLY, 0666);
+
+    char* buff = (char*)calloc(100, PID_SIZE);
+    int file_pid_index = read(lck_file, buff, 10);
+    buff[file_pid_index] = '\0';
+
+    return buff;
+}
+
+void sigint_handler(int signum, char* file_name) {
+    static char* lck_file_name = NULL;
+
+    if (lck_file_name == NULL) {
+        lck_file_name = file_name;
+    }
+    int pid = (int)getpid();
+    char str_pid[PID_SIZE];
+    sprintf(str_pid, "%d", pid);
+
+    if (signum == -1) {
+        return;
+    }
+
+    char* lck_file_pid = get_lck_file_str_pid(lck_file_name);
+    if (strcmp(lck_file_pid, str_pid) == 0) {
+        remove(lck_file_name);
+    }
+
+    FILE* result_file = fopen("result.txt", "a");
+    fprintf(result_file, "[pid:%d] всего блокировок - %d\n", pid, locks_count);
+    fclose(result_file);
+    exit(0);
+}
+
 int file_exists(char* filename) {
     return access(filename, F_OK) == 0;
 }
 
 void main(int argc, char** argv) {
+    signal(SIGINT, (void (*)(int))sigint_handler);
+
     if (argc != 2) {
         printf("Передано неверное количество параметров");
         return;
@@ -30,10 +70,11 @@ void main(int argc, char** argv) {
     char lck_file_name[MAX_FILENAME_LENGTH];
     sprintf(lck_file_name, "%s.lck", file_name);
 
+    sigint_handler(-1, lck_file_name);
+
     while (1) {
         if (file_exists(lck_file_name)) {
             sleep(0.01);
-            // printf("Файл %s уже используется другим процессом. Ожидание...\n", file_name);
             continue;
         }
 
@@ -41,22 +82,18 @@ void main(int argc, char** argv) {
         write(lck_file, str_pid, strlen(str_pid));
         close(lck_file);
         printf("Файл %s заблокирован для использования\n", file_name);
+        locks_count += 1;
 
         sleep(LOCK_TIME); // Какие-то действия с файлом
-
-        char* buff = (char*)calloc(100, PID_SIZE);
 
         if (!file_exists(lck_file_name)) {
             printf("Произошла ошибка\n");
             return;
         }
 
-        lck_file = open(lck_file_name, O_RDONLY, 0666);
+        char* lck_file_pid = get_lck_file_str_pid(lck_file_name);
 
-        int file_pid_index = read(lck_file, buff, 10);
-        buff[file_pid_index] = '\0';
-
-        if (strcmp(buff, str_pid) != 0) {
+        if (strcmp(lck_file_pid, str_pid) != 0) {
             printf("Произошла ошибка\n");
             close(lck_file);
             return;
