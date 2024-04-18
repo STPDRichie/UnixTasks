d026886b146f16dc819934bca41170566ec317e1
STPDRichie <nikolaj.karpov.2002@mail.ru>
2024-04-19 01:38:00 +0500

    в runme.sh дописал в принтах из скольки секунд прошло время

--- task2/runme.sh
+++ task2/runme.sh
@@ -13,21 +13,24 @@ touch file
 
 pids_list=()
 
+work_time=300
+
 for i in {1..10}
 do
     nohup ./lock file 2>/dev/null &
     pids_list+=($!)
 done
 
+echo ""
 ps -aux | grep ./lock
 echo ""
 
-echo -ne "Прошло секунд: 0"
-for i in {1..300}
+echo -ne "Прошло секунд: 0 из $work_time"
+for i in $( eval echo {1..$work_time} )
 do
     sleep 1
     echo -ne "\r"
-    echo -ne "Прошло секунд: $i"
+    echo -ne "Прошло секунд: $i из $work_time"
 done
 echo -e "\rРезультат записан в result.txt\n"
 
