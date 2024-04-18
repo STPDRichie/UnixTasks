f5747a28820b46a5c70bcfd06f891a68cde63b07
STPDRichie <nikolaj.karpov.2002@mail.ru>
2024-04-19 00:25:45 +0500

    написал runme.sh. добавил nohup.out в .gitignore. удалил file

---
+++ task2/runme.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+rm -f result.txt
+touch file
+
+pids_list=()
+
+for i in {1..10}
+do
+    nohup ./lock file 2>/dev/null &
+    pids_list+=($!)
+done
+
+ps -aux | grep ./lock
+echo ""
+
+echo -ne "Прошло секунд: 0"
+for i in {1..300}
+do
+    sleep 1
+    echo -ne "\r"
+    echo -ne "Прошло секунд: $i"
+done
+echo -e "\rРезультат записан в result.txt\n"
+
+for pid in ${pids_list[@]}
+do
+    kill -2 "$pid"
+done
+
+rm -f nohup.out
